d72c6f8f6b3af182e929b05e09efba33d2a90cd6
special0002 <159599382+special0002@users.noreply.github.com>
2024-03-14 00:16:12 +0530

    files uploaded

---
+++ 2.js
@@ -0,0 +1,7 @@
+function redirectToNextPage() {
+    window.location.href = "3.html"; // Change "3.html" to the URL of the page you want to redirect to
+}
+
+function redirectToWhatsAppPage() {
+    window.location.href = "5.html"; // Change "whatsapp.html" to the URL of the WhatsApp page
+}
